f973e47e50d644e38b8974ee2595c04c8794f953
richasingh13 <swtricha13@gmail.com>
2018-11-16 17:55:37 +0530

    initial setup

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,21 @@
+//= require rails-ujs
+//= require jquery3
+//= require site/bootstrap3.min
+//= require site/jquery.flexslider
+//= require site/jquery.themepunch.tools.min
+//= require site/jquery.themepunch.revolution.min
+//= require site/jquery.selectbox-0.1.3.min
+//= require site/jquery.magnific-popup
+//= require site/waypoints.min
+//= require site/jquery.counterup
+//= require site/wow.min
+//= require site/moment.min
+//= require site/fullcalendar.min
+//= require site/owl.carousel
+//= require site/jquery.syotimer
+//= require site/SmoothScroll
+//= require site/custom
+//= require site/optionswitcher
+//= require activestorage
+//= require turbolinks
+
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,59 @@
+class HomeController < ApplicationController
+  layout 'site'
+  def index; end
+
+  def about_us;end
+
+  def founder;end
+
+  def leadership;end
+
+  def sage;end
+
+  def university;end
+
+  def admission_procedure;end
+
+  def education_loan_information;end
+
+  def scholarships;end
+
+  def international_students_scholarships;end
+
+  def why_study_in_indore_india;end
+
+  def international_students_application_procedures;end
+
+  def international_linkage;end
+
+  def open_su;end
+
+  def campus_facilities;end
+
+  def student_services;end
+
+  def sports_recreation;end
+
+  def accommodation;end
+
+  def societies_special_interest_groups;end
+
+  def working_while_studying;end
+
+  def university_placement;end
+
+  def academic_model;end
+
+  def pedagogy_2;end
+
+  def program;end
+
+  def unique_approach_of_learning;end
+
+  def project_based_learning_system;end
+
+  def institutes;end
+
+  def contact;end
+
+end
